3b53d276dc72a38cadbfbc787a70a2586c89fd2c
joseguilherme-zg <zansavio.goncalves@escola.pr.gov.br>
2025-09-23 08:22:06 -0300

    update

--- dom_nilson.js
+++ dom_nilson.js
@@ -1,22 +1,47 @@
 function mudaCor(novaCor) {
-    var elemento = document.getElementById("para1");
-    elemento.style.backgroundColor = novaCor;
-    elemento.style.display = 'inline';
+   var elemento = document.getElementById("para1");
+   elemento.style.backgroundColor = novaCor;
+   elemento.style.display = 'inline';
 };
+
 
 const imagem = document.getElementById('foto')
 let foto_t = false
 
 
+
+
 imagem.addEventListener('click', ()=>{
-    if(foto_t === false){
-        imagem.src = 'pokedex.png';
-        foto_t = true
-    }else{
-        imagem.src = 'teste.jpg'
-        foto_t = false;
-    }
+   if(foto_t === false){
+       imagem.src = 'pokedex.png';
+       foto_t = true
+   }else{
+       imagem.src = 'teste.jpg'
+       foto_t = false;
+   }
 })
+
+
+const form = document.getElementById('texto')
+const section = document.getElementById('sc')
+
+
+form.addEventListener("submit", (event)=>{
+   event.preventDefault();
+   input=form.querySelector('input')
+   if(parseInt(input.value) < 24 ||isNaN(input.value) ){
+       var p = section.querySelector('p')
+       p.innerText = "Professor é mais velho"
+       p.style.display = 'block'
+   }else{
+       var p = section.querySelector('p')
+       p.innerHTML = '<h1> Olá, Mundo </h1>'
+       p.style.display = 'block'
+   }
+})
+
+
+
 
 
 
@@ -25,11 +50,13 @@ imagem.addEventListener('click', ()=>{
 
 
 const rodri_circulo = document.getElementById('circulo')
-let tamanho_rodri = 10
+let tamanho_rodri = 1
 
-rodri_circulo.addEventListener('dblclick',()=>{
-    rodri_circulo.style.borderRadius = `${tamanho_rodri}px`
-    rodri_circulo.style.width = `${tamanho_rodri}px`
-    rodri_circulo.style.height = `${tamanho_rodri}px`
-    tamanho_rodri = tamanho_rodri+20
-})
+
+rodri_circulo.addEventListener('dblclick',() => {
+   alert(rodri_tamanho)
+   rodri_circulo.style.borderRadius = `${tamanho_rodri}px`;
+   rodri_circulo.style.width = `${tamanho_rodri*2}px`;
+   rodri_circulo.style.height = `${tamanho_rodri*2}px`;
+   tamanho_rodri += 30;
+});
